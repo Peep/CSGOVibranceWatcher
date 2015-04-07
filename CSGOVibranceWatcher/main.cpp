3756369b2f851393cda6d02675f616366a08ea91
Corey Chase <peep@bmrf.me>
2015-04-07 16:00:46 -0300

    Some progress with the API, but it's not working yet. Proper documentation would help.

--- CSGOVibranceWatcher/main.cpp
+++ CSGOVibranceWatcher/main.cpp
@@ -2,16 +2,42 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <psapi.h>
+#include <string>
+#include <vector>
+#include <iostream>
 #include "adl_defines.h"
 #include "adl_sdk.h"
 #include "adl_structures.h"
 
+using namespace std;
+
 #define AMD 0
 #define NVIDIA 1
+
+typedef int(*ADL_MAIN_CONTROL_CREATE)(ADL_MAIN_MALLOC_CALLBACK, int);
+typedef int(*ADL_MAIN_CONTROL_DESTROY)();
+typedef int(*ADL_ADAPTER_PRIMARY_GET) (int*);
+typedef int(*ADL_DISPLAY_COLOR_GET) (int, int, int, int *, int *, int *, int *, int *);
+typedef int(*ADL_DISPLAY_COLOR_SET) (int, int, int, int);
+typedef int(*ADL_DISPLAY_DISPLAYINFO_GET) (int, int *, ADLDisplayInfo **, int);
+
+ADL_MAIN_CONTROL_CREATE			 ADL_Main_Control_Create;
+ADL_MAIN_CONTROL_DESTROY		 ADL_Main_Control_Destroy;
+ADL_ADAPTER_PRIMARY_GET			 ADL_Adapter_Primary_Get;
+ADL_DISPLAY_COLOR_GET			 ADL_Display_Color_Get;
+ADL_DISPLAY_COLOR_SET			 ADL_Display_Color_Set;
+ADL_DISPLAY_DISPLAYINFO_GET		 ADL_Display_DisplayInfo_Get;
 
 static int DisplayVendor;
 static bool ProcessDebugLogging = true;
 static bool DisplayDeviceDebugLogging = true;
+
+// Memory allocation function
+void* __stdcall ADL_Main_Memory_Alloc(int iSize)
+{
+	void* lpBuffer = malloc(iSize);
+	return lpBuffer;
+}
 
 bool ForegroundWindowIsCSGO()
 {
@@ -85,16 +111,66 @@ void GetDisplayDevice()
 	}
 }
 
-void main(void)
+int GetCurrentSaturation(int displayNum)
+{
+	int cur, def, min, max, step;
+	if (ADL_OK == ADL_Display_Color_Get(displayNum, 0, ADL_DISPLAY_COLOR_SATURATION, &cur, &def, &min, &max, &step))
+		return cur;
+}
+
+int main(void)
 {
 	GetDisplayDevice();
-	while (true)
+	if (DisplayVendor == AMD)
 	{
-		if (ForegroundWindowIsCSGO())
+		HINSTANCE hDLL = LoadLibraryA("atiadlxx.dll");
+		if (hDLL == NULL) // try 32 bit dll
+			hDLL = LoadLibraryA("atiadlxy.dll");
+		if (hDLL == NULL)
 		{
-			// do stuff
+			printf("ADL library not found!\n"); 
+			return 0;
 		}
-		Sleep(200);
+
+		ADL_Display_Color_Get = (ADL_DISPLAY_COLOR_GET)GetProcAddress(hDLL, "ADL_Display_Color_Get");
+		ADL_Display_Color_Set = (ADL_DISPLAY_COLOR_SET)GetProcAddress(hDLL, "ADL_Display_Color_Set");
+		ADL_Adapter_Primary_Get = (ADL_ADAPTER_PRIMARY_GET)GetProcAddress(hDLL, "ADL_Adapter_Primary_Get");
+		ADL_Display_DisplayInfo_Get = (ADL_DISPLAY_DISPLAYINFO_GET)GetProcAddress(hDLL, "ADL_Display_DisplayInfo_Get");
+
+		if (NULL == ADL_Display_Color_Get || NULL == ADL_Display_Color_Set)
+		{
+			printf("ADL's API is missing!\n");
+			return 1;
+		}
+
+		ADL_Main_Control_Create = (ADL_MAIN_CONTROL_CREATE)GetProcAddress(hDLL, "ADL_Main_Control_Create");
+		ADL_Display_Color_Get = (ADL_DISPLAY_COLOR_GET)GetProcAddress(hDLL, "ADL_Display_Color_Get");
+
+		if (ADL_OK != ADL_Main_Control_Create(ADL_Main_Memory_Alloc, 1))
+		{
+			printf("ADL Initialization Error!\n"); 
+			return 1;
+		}
+
+		int primary;
+		if (ADL_Adapter_Primary_Get(&primary) != ADL_OK)
+		{
+			printf("Couldn't retrieve primary display!\n");
+			return 1;
+		}
+
+		printf("Primary display is %d\n", primary);
+
+		printf("Current saturation for the primary monitor: %d\n", GetCurrentSaturation(primary));
 	}
+
+	//while (true)
+	//{
+	//	if (ForegroundWindowIsCSGO())
+	//	{
+	//		// do stuff
+	//	}
+	//	Sleep(200);
+	//}
 }
 
